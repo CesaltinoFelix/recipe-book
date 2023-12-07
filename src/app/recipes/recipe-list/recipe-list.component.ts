caf8dd42f42b65875a9df9b9b9a9d4aec4b1d030
cesaltinofelix <cesaltinofelix2000@gmail.com>
2023-12-07 16:34:33 +0100

    adding recipe-model on recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RecipeModel } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: RecipeModel[] = [
+    new RecipeModel('Arroz com feijao', 'Essa e a descricao da minha receita','https://www.bing.com/ck/a?!&&p=debabc9e71b7a991JmltdHM9MTcwMTkwNzIwMCZpZ3VpZD0yN2MwYjgyNS03ODRkLTY4YWUtM2MwNy1hYmY1NzlhZDY5ZTgmaW5zaWQ9NTU1NQ&ptn=3&ver=2&hsh=3&fclid=27c0b825-784d-68ae-3c07-abf579ad69e8&u=a1L2ltYWdlcy9zZWFyY2g_cT1mb29kIGltYWdlJkZPUk09SVFGUkJBJmlkPTIzRTA1RjgzQjlGREM2OTJGRkYzRDM1NTFGNTE5NTdCRDQ0MTNCMDI&ntb=1'),
+  ];
+
+
 
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+export class RecipeModel{
+
+public name: string;
+public description: string;
+public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string){
+            this.name = name;
+            this.description = description;
+            this.imagePath = imagePath;
+    }
+
+}
