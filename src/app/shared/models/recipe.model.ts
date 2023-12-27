c7bc9ec1413871e04a2559b3bffa07f3d0187a6d
cesaltinofelix <cesaltinofelix2000@gmail.com>
2023-12-27 08:46:45 +0100

    updating Recipe Model

--- src/app/shared/models/recipe.model.ts
+++ src/app/shared/models/recipe.model.ts
@@ -1,13 +1,17 @@
+import { IngredientModel } from "./ingredient.model";
+
 export class RecipeModel{
 
 public name: string;
 public description: string;
 public imagePath: string;
+public ingridients: IngredientModel[]
 
-    constructor(name: string, description: string, imagePath: string){
+    constructor(name: string, description: string, imagePath: string, ingredients: IngredientModel[]){
             this.name = name;
             this.description = description;
             this.imagePath = imagePath;
+            this.ingridients = ingredients;
     }
 
 }
